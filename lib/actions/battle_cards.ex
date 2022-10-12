deb524eebb6b8872c2ec602357e9b8ac51641bd7
Rafael Veiga <dsg.rafael@gmail.com>
2022-10-12 19:51:54 -0300

    improve card comparison

--- lib/actions/battle_cards.ex
+++ lib/actions/battle_cards.ex
@@ -5,7 +5,7 @@ defmodule CardsGame.Actions.BattleCards do
     player_card = draw_card()
     computer_card = draw_card()
 
-    compare_cards([player_card, computer_card])
+    compare_cards(player_card, computer_card)
   end
 
   def draw_card() do
@@ -20,11 +20,43 @@ defmodule CardsGame.Actions.BattleCards do
     hd(card)
   end
 
-  def compare_cards(card_list) do
-    card_list
-    |> Enum.map(&String.split(&1, "of"))
-    # why
-    |> Enum.map(fn s -> Enum.map(s, &String.split(&1)) end)
-    |> IO.inspect()
+  def compare_cards(player_card, computer_card) do
+    player_card = parse_card(player_card)
+    computer_card = parse_card(computer_card)
+
+    compare_numbers(map_to_integer(hd(player_card)), map_to_integer(hd(computer_card)))
+  end
+
+  def parse_card(card) do
+    card
+    |> String.split("of")
+    |> Enum.map(&String.trim(&1))
+  end
+
+  def compare_numbers(player_number, computer_number) do
+    cond do
+      player_number > computer_number -> {:player}
+      player_number < computer_number -> {:computer}
+      player_number == computer_number -> {:draw}
+    end
+  end
+
+  def map_to_integer(text_number) do
+    case text_number do
+      "Ace" -> 1
+      "Two" -> 2
+      "Three" -> 3
+      "Four" -> 4
+      "Five" -> 5
+      "Six" -> 6
+      "Seven" -> 7
+      "Eight" -> 8
+      "Nine" -> 9
+      "Ten" -> 10
+      "Jack" -> 11
+      "Queen" -> 12
+      "King" -> 13
+      _ -> nil
+    end
   end
 end
